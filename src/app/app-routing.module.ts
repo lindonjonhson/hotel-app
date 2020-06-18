fe21b080b6793a4b0050b23de4c51cdb0a85dc58
lindonjonhson <lindon.jonhsonf@gmail.com>
2020-06-18 16:40:50 +0100

    Added routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,26 @@
+import { SignupComponent } from './auth/signup/signup.component';
+import { SigninComponent } from './auth/signin/signin.component';
+import { OffersComponent } from './offers/offers.component';
+import { WeddingsComponent } from './weddings/weddings.component';
+import { LifestyleComponent } from './lifestyle/lifestyle.component';
+import { FoodAndBeverageComponent } from './food-and-beverage/food-and-beverage.component';
+import { CelebrationsComponent } from './celebrations/celebrations.component';
+import { AccomodationComponent } from './accomodation/accomodation.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'accomodation', component: AccomodationComponent},
+  { path: 'celebrations', component: CelebrationsComponent },
+  { path: 'food-and-beverage', component: FoodAndBeverageComponent },
+  { path: 'lifestyle', component: LifestyleComponent },
+  { path: 'weddings', component: WeddingsComponent },
+  { path: 'offers', component: OffersComponent },
+  { path: 'signin', component: SigninComponent },
+  { path: 'signup', component: SignupComponent }
+  // { path: '', component:  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
