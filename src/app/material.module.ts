5584d4e01400f6652cc0c39d6cdc5f7f495af676
lindonjonhson <lindon.jonhsonf@gmail.com>
2020-09-29 11:06:15 +0100

    Added toolbar and icons

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,15 +1,21 @@
 import { NgModule, Component } from '@angular/core';
 import {MatBadgeModule} from '@angular/material/badge';
 import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
     imports: [
         MatBadgeModule,
-        MatButtonModule
+        MatButtonModule,
+        MatToolbarModule,
+        MatIconModule
     ],
     exports: [
         MatBadgeModule,
-        MatButtonModule
+        MatButtonModule,
+        MatToolbarModule,
+        MatIconModule
     ]
 })
 export class MaterialModule { }
