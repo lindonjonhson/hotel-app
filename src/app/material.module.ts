3ec38ff908421a1beabe0c332d330a04d3780397
lindonjonhson <lindon.jonhsonf@gmail.com>
2020-09-30 11:26:07 +0100

    Further changes to sidenav structure

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -4,6 +4,7 @@ import {MatButtonModule} from '@angular/material/button';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list';
 
 @NgModule({
     imports: [
@@ -11,14 +12,16 @@ import {MatSidenavModule} from '@angular/material/sidenav';
         MatButtonModule,
         MatToolbarModule,
         MatIconModule,
-        MatSidenavModule
+        MatSidenavModule,
+        MatListModule
     ],
     exports: [
         MatBadgeModule,
         MatButtonModule,
         MatToolbarModule,
         MatIconModule,
-        MatSidenavModule
+        MatSidenavModule,
+        MatListModule
     ]
 })
 export class MaterialModule { }
